ce587b4a1874d2bd9aa45548ea6dc6b556e49526
Sylvia Sievers <ssievers3@gmail.com>
2016-04-09 19:01:24 -0400

    Assignment 38 signin completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
   ### the HTML and CSS to display the form element and any associated errors.
     content_tag :div, capture(&block), class: css_class
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
+  
 end
